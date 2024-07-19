743cfd8335106e139925d0445b92a5d3cb32b24c
Saravanan S <145825440+saravanan-RMKEC@users.noreply.github.com>
2024-07-19 21:07:31 +0530

    Add files via upload

---
+++ StudentGrading.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class StudentResults {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        
+        int[] marks = new int[numSubjects];
+        System.out.println("Enter the marks obtained in each subject (out of 100):");
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+        }
+        
+        int totalMarks = calculateTotalMarks(marks);
+        double averagePercentage = calculateAveragePercentage(totalMarks, numSubjects);
+        String grade = calculateGrade(averagePercentage);
+        
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage);
+        System.out.println("Grade: " + grade);
+    }
+
+    public static int calculateTotalMarks(int[] marks) {
+        int total = 0;
+        for (int mark : marks) {
+            total += mark;
+        }
+        return total;
+    }
+
+    public static double calculateAveragePercentage(int totalMarks, int numSubjects) {
+        return (double) totalMarks / numSubjects;
+    }
+
+    public static String calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return "A+";
+        } else if (averagePercentage >= 80) {
+            return "A";
+        } else if (averagePercentage >= 70) {
+            return "B";
+        } else if (averagePercentage >= 60) {
+            return "C";
+        } else if (averagePercentage >= 50) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
